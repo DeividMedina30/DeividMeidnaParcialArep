c83197d22a2b65b7ee74efbb97ee99f1e1ca1362
unknown <deivid.medina@labinfo.is.escuelaing.edu.co>
2022-02-23 16:38:20 -0500

    Creando clase getPort

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -2,70 +2,65 @@ package edu.escuelaing.arep;
 
 import java.net.*;
 import java.io.*;
-
+import java.util.*;
 
 
 public class HttpServer {
 
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
 
 
-    public static void main (String [ ] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = null;
+        boolean running = true;
+
         try {
-            serverSocket = new ServerSocket(getHerokuAssignedPort());
+            serverSocket = new ServerSocket(getPort()); //sOCKET DE SERVIDOR, PUERTO 35000
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
         }
 
-        boolean running = true;
-
         while (running) {
-            Socket clientSocket = null;
+
+            Socket clientSocket = null; //Cliente socket
             try {
                 System.out.println("Listo para recibir ...");
-                clientSocket = serverSocket.accept();
+                clientSocket = serverSocket.accept(); //Se pone a aceptar conexiones
             } catch (IOException e) {
                 System.err.println("Accept failed.");
                 System.exit(1);
             }
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); //Flujo de salida
+            BufferedReader in = new BufferedReader( //Flujo de entrada
+                    new InputStreamReader(clientSocket.getInputStream()));
             String inputLine, outputLine;
-            boolean primeraLinea = true;
-            String file = "";
+
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Received: " + inputLine);
-                if (primeraLinea) {
-                    file = inputLine.split(" ")[1];
-                    System.out.println("File: " + file);
-                    primeraLinea = false;
-                }
                 if (!in.ready()) {
                     break;
                 }
             }
 
-            if (file.startsWith("/Apps/")) {
-                //ds
-            } else {
-                outputLine = "HTTP/1.1 200 OK\r\n"
-                        + "Content-Type: text/html\r\n"
-                        + "\r\n"
-                        + "<!DOCTYPE html>"
-                        + "<html>"
-                        + "<head>"
-                        + "<meta charset=\"UTF-8\">"
-                        + "<title>No se encontro la pagina</title>\n"
-                        + "</head>"
-                        + "<body>"
-                        + "My Web Site"
-                        + "</body>"
-                        + "</html>";
-            }
+            //Siempre responde la misma página
+
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text /html\r\n"
+                    + "\r\n"
+                    + "<!DOCTYPE html>"
+                    + "<html>"
+                    + "<head>"
+                    + "<meta charset=\"UTF-8\">"
+                    + "<title>Title of the document</title>\n"
+                    + "</head>"
+                    + "<body>"
+                    + "Web Page"
+                    + "</body>"
+                    + "</html>" + inputLine;
+            out.println(outputLine);
             out.close();
             in.close();
             clientSocket.close();
@@ -73,15 +68,11 @@ public class HttpServer {
         serverSocket.close();
     }
 
-    /**
-     * Funcón que me permite obtener el puerto por el cual heroku despliega nuestra aplicación.
-     */
-    public	static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
         }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 
 }
